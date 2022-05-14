04a153f9088dd940a30fe345d411f9e22d917a6c
xyntechx <xyntechx@gmail.com>
2022-05-14 23:21:36 +0800

    ✨ Complete homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,25 @@
 <template>
     <main class="main">
-        <h1 class="title">Wordle</h1>
-        <p class="text">A Wordle Clone</p>
-        <NuxtLink to="/play">Play</NuxtLink>
-        <sub>Thank you
-            <a href="https://www.kaggle.com/datasets/shamiljamion/wordle-answers" target="_blank">shamiljamion</a>
-            for the list of words!
-        </sub>
-        <sub>Play the official Wordle game
-            <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank">
-                here
-            </a>
-        </sub>
+        <h1 class="title">A Wordle Clone by
+            <a href="https://xyntechx.com/" target="_blank" class="link">Nyx Iskandar</a>
+        </h1>
+        <p class="text">5 Letters at a Time</p>
+        <NuxtLink to="/play" class="button">Play</NuxtLink>
+        <footer class="acknowledgements">
+            <sub>|</sub>
+            <sub>Thank you
+                <a href="https://www.kaggle.com/datasets/shamiljamion/wordle-answers" target="_blank"
+                    class="link-sub">shamiljamion</a>
+                for the list of words
+            </sub>
+            <sub>|</sub>
+            <sub>Play the official Wordle game
+                <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank" class="link-sub">
+                    here
+                </a>
+            </sub>
+            <sub>|</sub>
+        </footer>
     </main>
 </template>
 
@@ -29,11 +37,81 @@
     font-size: 4rem;
     text-align: center;
     color: #00DC82;
+    margin: 1rem;
 }
 
 .text {
     font-size: 2rem;
     text-align: center;
     color: #80EEC0;
+    margin: 0 1rem;
+}
+
+.acknowledgements {
+    margin: 0 1rem;
+    position: fixed;
+    bottom: 10px;
+    color: white;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    column-gap: 2rem;
+    z-index: 1;
+}
+
+.acknowledgements sub {
+    font-size: 1rem;
+    text-align: center;
+}
+
+.button {
+    color: #80EEC0;
+    border: 0.3rem solid #80EEC0;
+    border-radius: 0.3rem;
+    font-size: 2rem;
+    text-decoration: none;
+    padding: 0.3rem 1rem;
+    margin: 1rem;
+    transition: 0.3s;
+}
+
+.button:hover {
+    background-color: #80EEC0;
+    color: #001F27;
+}
+
+.link {
+    color: #00DC82;
+    transition: 0.3s;
+}
+
+.link:hover {
+    border: 0.3rem solid #00DC82;
+    border-radius: 0.5rem;
+    text-decoration: none;
+    padding: 0.3rem 1rem;
+}
+
+.link-sub {
+    color: #00DC82;
+}
+
+@media (max-width: 800px) {
+    .title {
+        font-size: 2rem;
+    }
+
+    .text {
+        font-size: 1rem;
+    }
+
+    .acknowledgements {
+        column-gap: 0.5rem;
+    }
+
+    .acknowledgements sub {
+        font-size: 0.5rem;
+    }
 }
 </style>
